a41b5ce4fc620da99b48d41aab778d0c73778352
Barbara Kumor <b.kumor@yahoo.com>
2020-07-21 19:18:27 +0200

    router, methods

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+const exerciseRoutes = require('./routes/exerciseRoutes.js');
+const userRoutes = require('./routes/userRoutes.js');
+
 require('dotenv').config();
 
 const app = express();
@@ -12,7 +15,10 @@ app.use(express.json());
 
 const uri = process.env.MONGO_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
-    .then(() => app.listen(port, () => console.log(`Running on port ${port}`)))
+    .then(() => app.listen(port, () => console.log(`Running on port ${port}`)));
+
+app.use('/exercises', exerciseRoutes);
+app.use('/users', userRoutes);
 
 const connection = mongoose.connection;
 connection.once('open', () => console.group('Connected to MongDB'))
