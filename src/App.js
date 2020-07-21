d25b8409aaa13618ab74bc4ba17dc22fdb5f3e4b
Barbara Kumor <b.kumor@yahoo.com>
2020-07-21 19:58:04 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import ExerciseList from "./components/ExerciseList";
 import CreateExercise from "./components/CreateExercise";
+import EditExercise from "./components/EditExercise";
 import CreateUser from "./components/CreateUser";
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={ExerciseList}/>
-        <Route path="/edit/:id" component={EditExercise}/>
-        <Route path="/create" component={CreateExercise}/>
-        <Route path="/user" component={CreateUser}/>
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="container">
+                <Navbar/>
+                <Switch>
+                    <Route path="/" exact component={ExerciseList}/>
+                    <Route path="/edit/:id" component={EditExercise}/>
+                    <Route path="/create" component={CreateExercise}/>
+                    <Route path="/user" component={CreateUser}/>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
