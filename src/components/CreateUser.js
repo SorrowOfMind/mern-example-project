2f91bc4bd2e8ccef627edbf790deed8602b03c1f
Barbara Kumor <b.kumor@yahoo.com>
2020-07-22 18:36:38 +0200

    create user component

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
 
-export class CreateUser extends Component {
+class CreateUser extends Component {
+    state = {
+        username: ''
+    }
+
+    handleChange = e => this.setState({username: e.target.value})
+    
+    handleSubmit = e => {
+        e.preventDefault();
+        const newUser = this.state.username;
+        console.log(newUser);
+        this.setState({username: ''})
+    }
+
     render() {
+        const {username} = this.state;
         return (
-            <div>
-                create user
+            <div className="my-5">
+                <h3>Create New User</h3>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label>Username:</label>
+                        <input 
+                            type="text"
+                            required
+                            className="form-control"
+                            value={username}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-primary">Create New User</button>
+                    </div>
+                </form>
             </div>
         )
     }
